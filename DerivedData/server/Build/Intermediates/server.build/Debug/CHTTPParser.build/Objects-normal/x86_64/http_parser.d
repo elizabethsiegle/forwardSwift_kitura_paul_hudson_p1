69035b33afa0ccbba14684a5228b6f85f8d67b52
Elizabeth Siegle <lizzie.siegle@gmail.com>
2017-03-05 00:32:13 -0800

    pls

---
+++ DerivedData/server/Build/Intermediates/server.build/Debug/CHTTPParser.build/Objects-normal/x86_64/http_parser.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/lizziesiegle/Desktop/server/Packages/Kitura-net-1.6.2/Sources/CHTTPParser/http_parser.c \
+  /Users/lizziesiegle/Desktop/server/Packages/Kitura-net-1.6.2/Sources/CHTTPParser/include/http_parser.h
---
+++ DerivedData/server/Build/Intermediates/server.build/Debug/CHTTPParser.build/Objects-normal/x86_64/utils.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/lizziesiegle/Desktop/server/Packages/Kitura-net-1.6.2/Sources/CHTTPParser/utils.c \
+  /Users/lizziesiegle/Desktop/server/Packages/Kitura-net-1.6.2/Sources/CHTTPParser/include/utils.h \
+  /Users/lizziesiegle/Desktop/server/Packages/Kitura-net-1.6.2/Sources/CHTTPParser/include/http_parser.h
